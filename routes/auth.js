b93a8a53d74df8574577fafd590cd925e7b1f9f9
Randy Arteaga <randyarteaga1519@gmail.com>
2025-08-17 17:00:12 -0500

    Terminado prototipo

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,23 @@ router.post('/relogin', (req, res) => {
   return res.redirect('/dist/login.html');
   
 });
+// REDIRIGIR AL MAIN DE LOS USUARIOS
+router.post('/remesa', (req, res) => {
+
+  return res.redirect('/dist/mainUsuario.html');
+  
+});
+// REDIRIGIR AL Cronometro
+router.post('/recrono', (req, res) => {
+  return res.redirect('/dist/Cronometro.html');
+});
+// REDIRIGIR ALA PANTALLA DE PAGO
+router.post('/ppago', (req, res) => {
+  return res.redirect('/dist/pantallaPago.html');
+});
+router.post('/pantallaFinal', (req, res) => {
+  return res.redirect('/dist/pantallaFinal.html');
+});
 
 // REDIRIGIR AL INDEX
 router.post('/reindex', (req, res) => {
